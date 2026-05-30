e642e7124c5f70e96fef04163f6f4d8b79ef89b1
Belal-Elaiwa <Belal.3laiwa@gmail.com>
2026-05-30 21:03:24 +0300

    Control_Flow_Intro

---
+++ Control_Flow.py
@@ -0,0 +1,76 @@
+# How things can be executed in Python ?
+# what is Control flow ? Why use it ?
+
+
+# Control Flow:
+# It is the logic you write to controlhow your code runs 
+
+# Control Flow Tools: 
+"""
+Control flow Statements :
+     Special instructions in Python to change the flow 
+
+  [1]- Conditional Statement: (Make Decisions)
+         Used to make decisions
+         if , else, elif   
+  [2]- Loops: (Repeat Tasks)
+         Used to execute the same code multiple times
+         for, while        
+         break, continue, pass
+------------------------------------------------------
+Control Flow Boolean Expressions 
+     build questions (Conditions) Python can evaluate as "yes" or "no"
+     (True or False)
+   [1]- Values
+          True, False  
+   [2]- Functions
+          bool(), any(), all(), isinstance()
+   [3]- Comparison Operators
+          ==, !=, <, >, >=, <=
+   [4]- Logical Operators
+          and, or, not
+   [5]- Membership Operators 
+          in, not in
+   [6]- Identity Operators
+          is, is not
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
