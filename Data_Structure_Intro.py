27f5d1430f9895bd0c0faa8590186e7761ebe481
Belal-Elaiwa <Belal.3laiwa@gmail.com>
2026-06-14 17:22:22 +0300

    Data_Structure_Intro

---
+++ Data_Structure_Intro.py
@@ -0,0 +1,27 @@
+"""
+Data Structure:
+  - It is a way of organizing and storing data so it can be used efficiently
+  - 4 Built-In Data Structures:
+    list[]     Common           Square Brackets
+    tuple()    No Changes       Parentheses
+    set{}      Unique           Curly Brackets
+    dict{}     Key values       Curly Brackets
+Why data Structures ?
+  - Data is nicely organized
+  - Shorter code & Easy to operate  
+
+Tools & Methods used:
+  - In Python Built in Module There are Functions &  a class for each data structure
+  - Each class has its own specific methods 
+  - Ex : Tuple --> .count()  .index()
+         List  --> .append() .insert() .sort()   # ScreenShot
+
+Functions VS Methods
+Functions:
+  - Start with the function then the variable  len(costs)
+  - Functions will never manipulate the original value
+
+Methods:
+  - Start with the variable then the method    costs.remove(10)
+  - More risky --> manipulate the original value directly
+"""
